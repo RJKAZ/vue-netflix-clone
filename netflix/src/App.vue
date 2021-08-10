a0e3655a4290ab5cd9a0df1294ea46bcf001229d
RJ <rjkaz@comcast.net>
2021-08-09 20:07:39 -0400

    update, need to fix header bug

--- netflix/src/App.vue
+++ netflix/src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+    <nav class="lg:block bg-gray-900 bg-opacity-70 z-10">
+      <ul class="flex space-x-5 lg:space-x-8 items-center p-4">
+        <li class="font-bold text-3xl text-red-600">VueFlix</li>
+        <li v-for="link in navLinks" :key="link.name">
+          <router-link
+            :to="link.href"
+            class="font-bold text-sm lg:text-base text-white hover:text-app-green-1 transition ease-in-out duration-300"
+            >{{ link.name }}</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header
+  
   }
-}
+};
 </script>
 
 <style>
---
+++ netflix/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const navLinks = ref({
+      Home: {
+        name: "Home",
+        href: "/"
+      },
+      TvShows: {
+        name: "Shows",
+        href: "/about"
+      },
+      Series: {
+        name: "Series",
+        href: "#"
+      }
+    });
+    return { navLinks };
+  }
+};
+</script>
